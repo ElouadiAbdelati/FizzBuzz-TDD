7958a75ac98451f4b064debcde3d23d43c81cf14
EL ouadi Abdelati <Elouadi_Abdelati@outlook.fr>
2022-03-13 13:59:58 +0100

    fix typo

--- src/main/java/utils/ListFactory.java
+++ src/main/java/utils/ListFactory.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListFactory {
-    public  static List<String> numberToList(int number){
-        List<String> result = new ArrayList<>();
+    public  static List<Integer> numberToList(int number){
+        List<Integer> result = new ArrayList<>();
         for (int i = 1 ; i<= number ; i++) {
-            result.add(String.valueOf(i));
+            result.add(i);
         }
         return result;
     }
--- src/test/java/ListFactoryTest.java
+++ src/test/java/ListFactoryTest.java
@@ -8,8 +8,8 @@ public class ListFactoryTest {
     
     @Test
     public void shouldReturnArrayOf123WhenInputEqual3(){
-        List<String> result = ListFactory.numberToList(3);
-        List<String> expected = List.of("1", "2", "3");
+        List<Integer> result = ListFactory.numberToList(3);
+        List<Integer> expected = List.of(1, 2, 3);
         Assertions.assertEquals(expected,result);
     }
 }
