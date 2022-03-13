cc65c3d03fc2b6d6cddd5a99c38c971427643811
EL ouadi Abdelati <Elouadi_Abdelati@outlook.fr>
2022-03-13 14:34:49 +0100

    step 2

--- src/test/java/FizzBuzzEngineTest.java
+++ src/test/java/FizzBuzzEngineTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzEngineTest {
@@ -9,4 +11,14 @@ class FizzBuzzEngineTest {
         assertThrows(IllegalArgumentException.class, () -> FizzBuzzEngine.fizzBuzz(0));
     }
 
+    @Test
+    void shouldReturnListOf1And2AndFizzWhenInput3() {
+        assertEquals(List.of("1", "2", "Fizz"), FizzBuzzEngine.fizzBuzz(3));
+    }
+
+
+    @Test
+    void shouldReturnListOf1And2AndFizzAnd4AndBuzzAndFizzWhenInput6() {
+        assertEquals(List.of("1", "2", "Fizz", "4", "Buzz","Fizz"), FizzBuzzEngine.fizzBuzz(6));
+    }
 }
